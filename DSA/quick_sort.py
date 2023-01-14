ffd15dfd792cc901e1ee321c653a5cb97fdab861
Pymode-Dev <pymode000@gmail.com>
2023-01-14 02:48:40 +0100

    update docstring

--- DSA/quick_sort.py
+++ DSA/quick_sort.py
@@ -1,8 +1,25 @@
+"""
+QuickSort is an Algorithm that use the concept of
+partitioning and recursion.
+We first partition the unsorted, and we apply recursive function
+on the partitioned array.
+"""
+
+
 class QuickSort:
+    """
+    QuickSort Class
+    """
     def __init__(self, array: list) -> None:
         self.array = array
 
-    def _partition(self, left_pointer: int, right_pointer: int):
+    def _partition(self, left_pointer: int, right_pointer: int) -> int:
+        """
+        The module that handles the partition
+        :param left_pointer:
+        :param right_pointer:
+        :return: left_pointer: int
+        """
         pivot_index = right_pointer
         pivot_value = self.array[pivot_index]
 
@@ -24,7 +41,14 @@ class QuickSort:
         self.array[left_pointer], self.array[pivot_index] = self.array[pivot_index], self.array[left_pointer]
         return left_pointer
 
-    def quicksort(self, left_index: int, right_index: int):
+    def quicksort(self, left_index: int, right_index: int) -> list:
+        """
+        quicksort module which handles recursion to the left, and right
+        of the partitioned array.
+        :param left_index:
+        :param right_index:
+        :return: self.array: list
+        """
         if right_index - left_index <= 0:
             return None
 
